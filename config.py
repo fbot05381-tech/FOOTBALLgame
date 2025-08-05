7215676ad13f997bbe674f790c3cd750a1385b08
fbot05381-tech <fbot05381@gmail.com>
2025-08-06 04:27:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = 25021528  # Replace with your API ID
 API_HASH = "ea73f0e2e8595b85030aa59a11b3ef3b"
-BOT_TOKEN = "your_bot_token"
+BOT_TOKEN = "8349059640:AAGLKDeX1pJ1Z_iVLzx9Kz7zek2sTIKnHGk"
