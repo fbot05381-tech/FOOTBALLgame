fd5fc1b05de2b4cd063d883f44f97ea7b566e98b
fbot05381-tech <fbot05381@gmail.com>
2025-08-06 04:11:10 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,8 +1,33 @@
+import random
+
+# Football GIFs for animation
+GIFS = [
+    "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif",
+    "https://media.giphy.com/media/3oz8xKaR836UJOYeOc/giphy.gif",
+    # Add more GIFs if you want
+]
+
+def random_football_gif() -> str:
+    """
+    Returns a random football-related GIF URL.
+    """
+    return random.choice(GIFS)
+
+def referee_image() -> str:
+    """
+    Returns the referee image URL.
+    """
+    return "https://i.imgur.com/jySqKyz.jpg"
+
+def organiser_image() -> str:
+    """
+    Returns the organiser image URL.
+    """
+    return "https://i.imgur.com/zL3Tb7T.jpg"
+
 def get_player_position(username: str) -> tuple[int, int]:
     if not username:
         username = "default"
-
     x = sum(ord(c) for c in username) % 10  # X coordinate: 0-9
     y = len(username) % 6                   # Y coordinate: 0-5
-
     return x, y
