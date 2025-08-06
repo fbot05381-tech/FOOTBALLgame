8ca24607dd902dc69a452229b453371e3deb936e
fbot05381-tech <fbot05381@gmail.com>
2025-08-06 18:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Client(
-    "football_bot",
+    "football_bot2",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
